5a4add4ec90ade6f304f83fd96694354d38c2e6f
Dima <kuzmichman92@gmail.com>
2021-10-15 02:55:02 +0300

    parse_commit_3

--- app/controllers/personal_parsers_controller.rb
+++ app/controllers/personal_parsers_controller.rb
@@ -1,10 +1,35 @@
 # frozen_string_literal: true
 
 class PersonalParsersController < ApplicationController
+  before_action :load_entities
+
   def index
-    doc = Nokogiri::HTML(URI.open('https://axela-app.herokuapp.com/'))
-    doc.css('title').each do |link|
-      puts link.content
+    @parsers = PersonalParser.all
+  end
+
+  def new
+    @parser = PersonalParser.new
+  end
+
+  def create
+    @parser = PersonalParser.new permitted_parameters
+
+    if @parser.save
+      redirect_to personal_parsers_path
+    else
+      render :new
     end
   end
+
+  protected
+
+  def load_entities
+    @parsers = PersonalParser.all
+    @parser = PersonalParser.find(params[:id]) if params[:id]
+  end
+
+  def permitted_parameters
+    params.require(:personal_parser).permit(:name, :parsed_url, :parsed_info)
+  end
 end
+
--- db/migrate/20211014204324_create_personal_parsers.rb
+++ db/migrate/20211014204324_create_personal_parsers.rb
@@ -4,7 +4,8 @@ class CreatePersonalParsers < ActiveRecord::Migration[5.2]
   def change
     create_table :personal_parsers do |t|
       t.string :name
-
+      t.string :parsed_url
+      t.string :parsed_info
       t.timestamps
     end
     add_index :personal_parsers, :name, unique: true
