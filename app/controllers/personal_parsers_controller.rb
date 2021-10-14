8eb1baa0c50ab7dffe6af77aaa499e9703903d72
Dima <kuzmichman92@gmail.com>
2021-10-15 02:22:41 +0300

    parse_commit_3

--- app/controllers/personal_parsers_controller.rb
+++ app/controllers/personal_parsers_controller.rb
@@ -3,8 +3,6 @@
 class PersonalParsersController < ApplicationController
   def index
     doc = Nokogiri::HTML(URI.open('https://axela-app.herokuapp.com/'))
-
-    # Search for nodes by css
     doc.css('title').each do |link|
       puts link.content
     end
